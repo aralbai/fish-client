636e299dc8d97acf3d9a5081b569bef0c1629f9b
xabibullayev <xabibullayevmm@gmail.com>
2025-03-29 02:26:39 +0500

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-import withPWA from "next-pwa";
-
-const nextConfig = {
-  distDir: "build",
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-};
-
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
